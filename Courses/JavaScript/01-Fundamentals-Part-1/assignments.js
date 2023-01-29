70ff8ec5c506f43166d35cfdd1b94dda629c23e2
nastaj <G00424689@atu.ie>
2023-01-29 13:16:49 +0000

    Values, Variables & Data Types

---
+++ Courses/JavaScript/01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,22 @@
+// *****************************
+// LECTURE: Values and variables
+// *****************************
+
+let country = "Poland";
+let continent = "Europe";
+let population = 38000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// *****************************
+// LECTURE: Data Types
+// *****************************
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- Courses/JavaScript/01-Fundamentals-Part-1/script.js
+++ Courses/JavaScript/01-Fundamentals-Part-1/script.js
@@ -1,5 +1,52 @@
+/*
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jakub");
+console.log(23);
+
+let firstName = "Jakub";
+let surname = "Nasta";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name convention
+let jakub_nasta = "JN";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+console.log(firstName + " " + surname);
+*/
+
+true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jakub");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
