6a95aed063c3526bcb57e355229d13209d292dd4
nastaj <G00424689@atu.ie>
2023-03-06 23:03:00 +0000

    Rest Pattern

--- Courses/JavaScript/09-Data-Structures-Operators/script.js
+++ Courses/JavaScript/09-Data-Structures-Operators/script.js
@@ -59,11 +59,60 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    if (otherIngredients.length > 3) {
+      console.log('Too many ingredients!');
+    } else {
+      console.log(`Ordered pizza with ${mainIngredient} 
+      ${otherIngredients}`);
+    }
+  },
 };
+
 ///////////////////////////////
-//Lecture: SPREAD OPERATOR
+//Lecture: REST PATTERN AND PARAMETERS
 ///////////////////////////////
 
+// 1) Destructuring
+// SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+///////////////////////////////
+//Lecture: SPREAD OPERATOR (...)
+///////////////////////////////
+/*
 // Expanding an array
 const arr = [7, 8, 9];
 
@@ -114,7 +163,7 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
-
+*/
 ///////////////////////////////
 //Lecture: DESTRUCTURING OBJECTS
 ///////////////////////////////
