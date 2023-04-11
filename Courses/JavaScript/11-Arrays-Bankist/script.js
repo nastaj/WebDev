d4eec89e2d09d48c7867a4e65693b276fabadcd7
nastaj <G00424689@atu.ie>
2023-04-11 13:58:28 +0100

    Creating and filling arrays

--- Courses/JavaScript/11-Arrays-Bankist/script.js
+++ Courses/JavaScript/11-Arrays-Bankist/script.js
@@ -567,7 +567,7 @@ console.log(overallBalance);
 /////////////////////////////////////////////////
 // Lecture: Sorting Arrays
 /////////////////////////////////////////////////
-
+/*
 // Strings
 const owners = ['Jakub', 'Zach', 'Adam', 'Martha'];
 
@@ -605,3 +605,41 @@ console.log(movements);
 movements.sort((a, b) => b - a);
 
 console.log(movements);
+*/
+/////////////////////////////////////////////////
+// Lecture: Creating and filling arrays
+/////////////////////////////////////////////////
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 4, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+const diceRolls = Array.from(
+  { length: 100 },
+  () => Math.trunc(Math.random() * 6) + 1
+);
+console.log(diceRolls);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+
+  console.log(movementsUI);
+});
