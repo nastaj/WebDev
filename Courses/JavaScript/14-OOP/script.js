614dc6c4f3d326d21be5566587b1c1d05b1e21ea
nastaj <G00424689@atu.ie>
2023-05-24 16:52:43 +0100

    OOP

--- Courses/JavaScript/14-OOP/script.js
+++ Courses/JavaScript/14-OOP/script.js
@@ -314,32 +314,87 @@
 // Lecture: Inheritance between "Classes": Object.create
 /////////////////////////////////
 
-const PersonProto = {
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  },
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   },
 
-  init(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  },
-};
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
 
-const steven = Object.create(PersonProto);
+// const steven = Object.create(PersonProto);
 
-const StudentProto = Object.create(PersonProto);
-StudentProto.init = function (firstName, birthYear, course) {
-  PersonProto.init.call(this, firstName, birthYear);
-  this.course = course;
-};
+// const StudentProto = Object.create(PersonProto);
+// StudentProto.init = function (firstName, birthYear, course) {
+//   PersonProto.init.call(this, firstName, birthYear);
+//   this.course = course;
+// };
 
-StudentProto.introduce = function () {
-  console.log(`My name is ${this.firstName} and I study ${this.course}`);
-};
+// StudentProto.introduce = function () {
+//   console.log(`My name is ${this.firstName} and I study ${this.course}`);
+// };
 
-const jay = Object.create(StudentProto);
-jay.init('Jay', 2010, 'Computer Science');
-jay.introduce();
-jay.calcAge();
+// const jay = Object.create(StudentProto);
+// jay.init('Jay', 2010, 'Computer Science');
+// jay.introduce();
+// jay.calcAge();
 
-console.log(jay);
+// console.log(jay);
+
+/////////////////////////////////
+// Lecture: Another Class Example
+/////////////////////////////////
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface (API)
+  deposit(value) {
+    this.movements.push(value);
+  }
+
+  withdraw(value) {
+    this.deposit(-value);
+  }
+
+  approveLoan(value) {
+    const accBalance = this.movements.reduce((acc, mov) => acc + mov, 0);
+
+    if (accBalance < value) console.log('You cannot take the loan!');
+    else return true;
+  }
+
+  currentBalance() {
+    const accBalance = this.movements.reduce((acc, mov) => acc + mov, 0);
+    console.log(`Current account balance: ${accBalance}`);
+  }
+
+  requestLoan(value) {
+    if (this.approveLoan(value)) {
+      this.deposit(value);
+      console.log('Loan approved');
+    }
+  }
+}
+
+const acc1 = new Account('Jakub', 'PLN', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.currentBalance();
+acc1.requestLoan(100);
+
+console.log(acc1);
