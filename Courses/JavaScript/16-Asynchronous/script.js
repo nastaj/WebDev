cb944bb6fe7de41bf0929c78fe56249a58525fd3
nastaj <G00424689@atu.ie>
2023-06-16 08:36:11 +0200

    Promise combinator functions

--- Courses/JavaScript/16-Asynchronous/script.js
+++ Courses/JavaScript/16-Asynchronous/script.js
@@ -143,14 +143,14 @@ const getCountryNeighbours = function (neighbours) {
 //     });
 // };
 
-// const getJSON = function (url, errorMsg = 'Something went wrong') {
-//   return fetch(url).then(response => {
-//     if (!response.ok) {
-//       throw new Error(`Country not found (${response.status})`);
-//     }
-//     return response.json();
-//   });
-// };
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(`Country not found (${response.status})`);
+    }
+    return response.json();
+  });
+};
 
 // const getCountryData = function (country) {
 //   // Country 1
@@ -255,26 +255,87 @@ const whereAmI = async function () {
     const [data] = await response.json();
     renderCountry(data);
 
-    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+    // return `You are in ${dataGeo.city}, ${dataGeo.country}`;
   } catch (err) {
     console.error(`${err} 🤯`);
     renderError(`🤯 ${err.message}`);
 
     // Reject promise returned from async function
-    throw err;
+    // throw err;
   }
 };
 
-console.log('1: Will get location');
-// const city = whereAmI();
-// console.log(city);
+whereAmI();
 
-(async function () {
+// console.log('1: Will get location');
+// // const city = whereAmI();
+// // console.log(city);
+
+// (async function () {
+//   try {
+//     const city = await whereAmI();
+//     console.log(`2: ${city}`);
+//     console.log(`3: Finished getting location`);
+//   } catch (err) {
+//     console.error(`2: ${err.message}`);
+//   }
+// })();
+
+const get3Countries = async function (c1, c2, c3) {
   try {
-    const city = await whereAmI();
-    console.log(`2: ${city}`);
-    console.log(`3: Finished getting location`);
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
   } catch (err) {
-    console.error(`2: ${err.message}`);
+    console.error(err);
   }
+};
+
+get3Countries('Poland', 'Germany', 'USA');
+
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v3.1/name/italy`),
+    getJSON(`https://restcountries.com/v3.1/name/egypt`),
+    getJSON(`https://restcountries.com/v3.1/name/mexico`),
+  ]);
+  console.log(res[0]);
 })();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(() => {
+      reject(new Error('Request took too long!'));
+    }, sec);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.com/v3.1/name/tanzania`),
+  timeout(1),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+]).then(res => console.log(res));
+
+// Promise.any
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+]).then(res => console.log(res));
